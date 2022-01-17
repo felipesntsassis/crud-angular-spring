1c31f07c5ec16b1e8063fcd6c7ede6b7ff00670c
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2022-01-17 20:03:53 -0300

    Desenvolvimento da tabela para listar os cursos.

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
@@ -10,6 +11,7 @@ import { CoursesComponent } from './courses/courses.component';
     CoursesComponent
   ],
   imports: [
+    AppMaterialModule,
     CommonModule,
     CoursesRoutingModule
   ]
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' },
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    // Também está certo! Mas poupa uma linha de código
+    // this.courses = [];
+  }
 
   ngOnInit(): void {
   }
-
 }
