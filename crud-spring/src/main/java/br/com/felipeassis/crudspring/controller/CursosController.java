1cc696acaf79a1d133a7672f37586ca932940449
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-01-18 07:39:16 -0300

    Criação do método get para obter o registro do curso.

--- crud-spring/src/main/java/br/com/felipeassis/crudspring/controller/CursosController.java
+++ crud-spring/src/main/java/br/com/felipeassis/crudspring/controller/CursosController.java
@@ -3,9 +3,9 @@ package br.com.felipeassis.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +21,13 @@ import lombok.AllArgsConstructor;
 public class CursosController {
     
     private final CourseRepository repository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return repository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+    }
 
     @GetMapping
     public List<Course> list() {
